e5148a13004572b247cdc1a041cd8fe2a87bacfa
Dusan Nikolic <dusan.nikolic@student.uib.no>
2021-02-09 22:12:17 +0100

    Class Tile modification

--- src/main/java/Objects/Tile.java
+++ src/main/java/Objects/Tile.java
@@ -2,5 +2,17 @@ package Objects;
 
 public class Tile implements IComponent{
 
+    private String name = "Tile";
+    private final char SYMBOL = 'T';
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public char getSymbol() {
+        return SYMBOL;
+    }
 }
 

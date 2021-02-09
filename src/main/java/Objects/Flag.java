a6793a43f3a5f0e40cd5d4ef9c42a98496ffa60b
Dusan Nikolic <dusan.nikolic@student.uib.no>
2021-02-09 22:04:18 +0100

    Class Flag is almost done. Initialized class Tile

--- src/main/java/Objects/Flag.java
+++ src/main/java/Objects/Flag.java
@@ -2,5 +2,20 @@ package Objects;
 
 public class Flag implements IComponent {
 
-    
+    String name;
+    char SYMBOL = 'F';
+
+    Flag(String name){
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public char getSymbol() {
+        return SYMBOL;
+    }
 }
--- src/main/java/Objects/IComponent.java
+++ src/main/java/Objects/IComponent.java
@@ -3,8 +3,8 @@ package Objects;
 
 /**
  * General explanation:
- * Every non-player component in our game is supposed to be in type of specific tile.
- * Therefore, to be able to place different objects on the same tile, we have chosen to represent
+ * Every component in our game is supposed to be in one type of specific position.
+ * Therefore, to be able to place different objects in the same position, we have chosen to represent
  * the board in 4 different layers --> grids.
  *
  * Some objects will be placed on tiles in the background layer, some in the foreground layer and some in the
@@ -21,6 +21,7 @@ public interface IComponent {
     /**
      *@return the symbol which is a character that represents a board-component on the board
      */
+    //TODO: Maybe unnessecary if we are just going to store the components in their objectform
     char getSymbol();
 
 }

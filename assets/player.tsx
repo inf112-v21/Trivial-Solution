2eb3c41b33c969bb4d4b9129a7a673f5dfc8902a
Dusan Nikolic <dusan.nikolic@student.uib.no>
2021-02-09 16:31:48 +0100

    Testmappe ferdiglaget i test map editor

--- assets/player.tsx
+++ assets/player.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="player" tilewidth="299" tileheight="300" tilecount="3" columns="3">
+<tileset version="1.4" tiledversion="1.4.3" name="player" tilewidth="300" tileheight="300" tilecount="3" columns="3">
  <image source="player.png" width="900" height="300"/>
 </tileset>

978f7e59d04ef97c23fd4da2f214174903edfa07
Samueljacoo <samuel.j.cook@gmail.com>
2021-02-10 12:14:29 +0100

    changed a method in Deck, reordered the constructor in Deck to be in order.

--- src/main/java/Cards/Deck.java
+++ src/main/java/Cards/Deck.java
@@ -7,6 +7,13 @@ public class Deck {
     int counter;
     public Deck(){
         for (int i=0; i<6; i++){
+            cards.add(new ProgramCard(0,2,i+1));
+            cards.add(new ProgramCard(0,-1,7+(2*i)));
+            cards.add(new ProgramCard(0,1,8+(2*i)));
+            cards.add(new ProgramCard(0,-1,19+(2*i)));
+            cards.add(new ProgramCard(0,1,20+(2*i)));
+            cards.add(new ProgramCard(0,-1,31+(2*i)));
+            cards.add(new ProgramCard(0,1,32+(2*i)));
             cards.add(new ProgramCard(-1,0,43+i));
             cards.add(new ProgramCard(1,0,49+i));
             cards.add(new ProgramCard(1,0,55+i));
@@ -14,19 +21,11 @@ public class Deck {
             cards.add(new ProgramCard(2,0,67+i));
             cards.add(new ProgramCard(2,0,63+i));
             cards.add(new ProgramCard(3,0,79+i));
-            cards.add(new ProgramCard(0,-1,7+(2*i)));
-            cards.add(new ProgramCard(0,1,8+(2*i)));
-            cards.add(new ProgramCard(0,-1,19+(2*i)));
-            cards.add(new ProgramCard(0,1,20+(2*i)));
-            cards.add(new ProgramCard(0,-1,31+(2*i)));
-            cards.add(new ProgramCard(0,1,32+(2*i)));
-            cards.add(new ProgramCard(0,2,i+1));
         }
     }
-    public ArrayList<ICard> shuffledDeck(){
+    public void shuffledDeck(){
         counter = 0;
         Collections.shuffle(cards);
-        return cards;
     }
 
     public ICard drawCard() {

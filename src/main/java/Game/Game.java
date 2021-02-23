3648b5cf60dabbfd39e05678cab7547dff4dd941
Samueljacoo <samuel.j.cook@gmail.com>
2021-02-23 16:30:19 +0100

    Uferdig Game-klasse men noen metode erdringer og lagt til en metode

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -16,14 +16,15 @@ public class Game {
     ArrayList<Robot> bots = new ArrayList<>();
     ArrayList<Register> registers = new ArrayList<>();
     ArrayList<ICard> tempRegister = new ArrayList<>();
+    ArrayList<ArrayList<ICard>> phaseRegisters = new ArrayList<>();
     Deck Deck = new Deck();
 
-    public void Game(int players){
+    public void Game(int players, String mapname){
         numberOfPlayers = players;
         for (int i=0; i < numberOfPlayers; i++){
             String name = "Player " +i+1;
             bots.add(new Robot(name, colours[i]));
-            registers.add(new Register());
+            registers.add(new Register(10,10,false));
         }
 
     }
@@ -37,7 +38,45 @@ public class Game {
             tempRegister.clear();
         }
     }
-    public void turn(){
+    public void phase(){
+        ArrayList<ICard> orderedCards = new ArrayList<>();
+        ArrayList<Robot> botOrder = new ArrayList<>();
+        //getRegisterCards is placeholder for 5 cars the players want to play
+        for (int i = 0; i< registers.size(); i++){
+            phaseRegisters.add(registers.get(i).getRegisterCards());
+        }
+        for(int turnCard = 0; turnCard < 5; turnCard++){
+            for (int r = 0; r < phaseRegisters.size(); r++){
+                int pri = 0;
+                ICard order = null;
+                Robot botInUse = null;
+                for(int registerInUse = 0; registerInUse < phaseRegisters.size(); registerInUse++){
+                    ArrayList<ICard> shortlist = phaseRegisters.get(registerInUse);
+                    if(shortlist.get(turnCard) == null){
+                        break;
+                    }
+                    if (orderedCards.contains(shortlist.get(turnCard))){
+                        break;
+                    }
+                    if(pri < shortlist.get(turnCard).priority()){
+                        pri = shortlist.get(turnCard).priority();
+                        order = shortlist.get(turnCard);
+                        botInUse = bots.get(registerInUse);
+                        shortlist.remove(turnCard);
+                    }
+                    else{
+                        //temp
+                        break;
+                    }
+                }
+                orderedCards.add(order);
+                botOrder.add(botInUse);
+            }
+        }
+        for (int p = 0; p < orderedCards.size(); p++){
+            //board.performove(orderedCards.get(p), botOrder.get(p))
+        }
+
     }
     public void endRound(){
     }

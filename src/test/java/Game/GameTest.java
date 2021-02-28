a05d603e2edd23c7d4507eebbd99d936799d8dcf
Dusan Nikolic <dusan.nikolic@student.uib.no>
2021-02-28 11:38:58 +0100

    La til GameTest klassen

---
+++ src/test/java/Game/GameTest.java
@@ -0,0 +1,28 @@
+package Game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void startRound() {
+    }
+
+    @Test
+    void phase() {
+    }
+
+    @Test
+    void endRound() {
+    }
+
+    @Test
+    void destroyedBot() {
+    }
+
+    @Test
+    void hasWon() {
+    }
+}

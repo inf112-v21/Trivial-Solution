293f7c9b806963aacaa3f98e9e7c0ba514504298
Dusan Nikolic <dusan.nikolic@student.uib.no>
2021-03-31 13:57:25 +0200

    Begynte med gameflow og refactoring

---
+++ src/main/java/NetworkMultiplayer/Messages/ConfirmationMessages.java
@@ -0,0 +1,7 @@
+package NetworkMultiplayer.Messages;
+
+public enum ConfirmationMessages implements IMessage{
+    CONNECTION_WAS_SUCCESSFUL,
+    GAME_WAS_STARTED_AND_CLIENT_IS_READY_TO_RECEIVE_CARDS,
+    SIMULATION_IS_OVER
+}

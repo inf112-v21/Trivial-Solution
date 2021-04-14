42300d34789747e7842ab7c9e398dd0a7fcc017d
Dusan Nikolic <dusan.nikolic@student.uib.no>
2021-04-14 15:32:23 +0200

    MultiplayerTest. Main kalles fortsatt to ganger.

--- src/test/java/TestClasses/MultiplayerGUITest.java
+++ src/test/java/TestClasses/MultiplayerGUITest.java
@@ -4,6 +4,7 @@ import GUIMain.GUI;
 import GUIMain.Main;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.lwjgl.system.CallbackI;
 
 
 /**
@@ -11,58 +12,39 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
  * Får å sjekke at det virker så kjører du host klassen først.
  *
  * 1. Trykk MUltiplayer knappen og deretter velg å være host.
- * 2. Kjør client1/2 og velg join i mulitplayer.
+ * 2. Kjør client1 og velg join i mulitplayer.
  *
  * Deretter kan man velge flere ting...
  */
+public class MultiplayerGUITest {
 
+    static class Host{
+        public static void main(String[] args) {
+            Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+            cfg.setTitle("Robo-Rally");
+            cfg.setAutoIconify(true);
+            cfg.setResizable(false);
 
-
-public class MultiplayerGUITest{
-
-    public static void startProgram(){
-        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("Robo-Rally");
-        cfg.setAutoIconify(true);
-        cfg.setResizable(false);
-
-        new Lwjgl3Application(new GUI(), cfg);
-    }
-
-    public static void main(String[] args) {
-
-        Thread Host = new Thread(new host());
-        Thread Client1 = new Thread(new client1());
-        Thread Client2 = new Thread(new client2());
-
-        Host.start();
-        Client1.start();
-        Client2.start();
-
-    }
-
-    static class host implements Runnable{
-
-        @Override
-        public void run() {
-            startProgram();
+            new Lwjgl3Application(new GUI(), cfg);
         }
     }
 
+    static class Client1{
+        public static void main(String[] args) {
+            Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+            cfg.setTitle("Robo-Rally");
+            cfg.setAutoIconify(true);
+            cfg.setResizable(false);
 
-    static class client1 implements Runnable{
-
-        @Override
-        public void run() {
-            startProgram();
+            new Lwjgl3Application(new GUI(), cfg);
         }
     }
 
-    static class client2 implements Runnable{
-
-        @Override
-        public void run() {
-            startProgram();
-        }
-    }
 }
+
+
+
+
+
+
+

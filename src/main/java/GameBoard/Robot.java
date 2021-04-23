97aa7cdc4f623213fedd31c44d13a9eaae01669b
sco008 <Samuel.Cook@student.uib.no>
2021-04-23 16:03:09 +0200

    kortene kan nå ses

--- src/main/java/GameBoard/Robot.java
+++ src/main/java/GameBoard/Robot.java
@@ -161,11 +161,13 @@ public class Robot implements Serializable, Comparable<Robot> {
 	/**
 	 * Denne metoden legger til et og et kort i rekkefølge i registeret utifra hva spilleren velger.
 	 * @param chosenCard kortet som ble valgt
+	 * @return
 	 */
-	public void chooseCard(ProgramCard chosenCard){
+	public boolean chooseCard(ProgramCard chosenCard){
 	    if (! availableCards.contains(chosenCard)) throw new IllegalArgumentException("This card isn't in the list of available cards for som reason");
 	    if (chosenCards.size() >= getChosenCardSlots()) throw new IllegalStateException("I already have the maximum number of cards!");
 		chosenCards.add(chosenCard);
+		return true;
 	}
 
 	public int getNumberOfChosenCards(){ return chosenCards.size(); }
